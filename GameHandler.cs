bf8284c32cfe657f70e0876b44e21f053dfbe878
mtimcenko <55092300+mtimcenko@users.noreply.github.com>
2019-11-20 12:13:46 -0500

    Player movement with camera follow.
    Attach the GameHandler to the main camera, and add the player's transform under the player transform and the main camera to the camera follow.

---
+++ GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public CameraFollow cameraFollow;
+
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    
+    //attatch this to the games main camera.
+    void Start()
+    {
+        cameraFollow.Setup(()=>playerTransform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
