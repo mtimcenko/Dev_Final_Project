cabe17b8f64e6a4a544f26118c7639478ad02391
Mike <mikelyzhang99@gmail.com>
2019-11-23 14:24:50 -0500

    Starting Main Scene

--- TESTING/test/Assets/MikeFolder/PlayerController.cs
+++ TESTING/test/Assets/MikeFolder/PlayerController.cs
@@ -4,46 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+//    Rigidbody2D body;
 
     public Vector2 velocity;
+    public Vector2 velocityY;
 
     public float speed = 5.0f;
 
     public float groundDeceleration = 0.5f;
 
     public float walkAcceleration = 0.5f;
+//    
+//    float horizontal;
+//    float vertical;
+//    
+//    public float runSpeed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // CameraFollow.Setup(new Vector3(0, -100));
+    }
 
+    // Update is called once per frame
+  
+    
     void Update()
     {
         
         float moveInput = Input.GetAxisRaw("Horizontal");
-        //if the player is moving right or left
+        
         if (moveInput != 0)
         {
             velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
         }
-        //if they're not inputting anything
         else
         {
             velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
-
         }
 
         float moveInputVertical = Input.GetAxis("Vertical");
-    //if they're moving vertically
+
         if (moveInputVertical != 0)
         {
             velocity.y = Mathf.MoveTowards(velocity.y, speed * moveInputVertical, walkAcceleration * Time.deltaTime);
         }
-        //if they're not inputting anything
         else
         {
-
             velocity.y = Mathf.MoveTowards(velocity.y, 0, groundDeceleration * Time.deltaTime);
         }
         
         transform.Translate(velocity * Time.deltaTime);
+       //transform.Translate(velocity*Time.deltaTime);
+        
+        
 
+//        horizontal = Input.GetAxisRaw("Horizontal");
+//        vertical = Input.GetAxisRaw("Vertical"); 
     }
 
+    private void FixedUpdate()
+    {
+       // body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
 }
